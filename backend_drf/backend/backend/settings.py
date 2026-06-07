969ec03be35a284119e1d763b636b7bc4aaba977
Ghulam Abrar <gh_abrar51@yahoo.co.id>
2026-06-07 15:29:36 +0900

    Allow railway backend to the CORS_ALLOWED_ORIGIN

--- backend_drf/backend/backend/settings.py
+++ backend_drf/backend/backend/settings.py
@@ -155,6 +155,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:4173",
     "http://127.0.0.1:4173",
+    "https://vanillaperformancemanagement-production.up.railway.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
